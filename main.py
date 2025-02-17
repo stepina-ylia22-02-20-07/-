dd6e5fd48af9457a09debe995dbee4d3120cfb1d
stepina-ylia22-02-20-07 <stepinaulia11@gmail.com>
2025-02-17 16:07:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+import sys
+
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(500, 500, 200, 200)
+        self.setWindowTitle("Git и желтые окружности")
+        self.btn = QPushButton('Рисовать', self)
+        self.btn.move(70, 150)
+        self.btn.resize(60, 40)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        a = random.randint(10, 100)
+        r = random.randint(0, 255)
+        qp.setBrush(QColor(225, 225, 0))
+        qp.drawEllipse(random.randint(0, 200), random.randint(0, 200), a, a)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
